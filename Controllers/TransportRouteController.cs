877c08e7b11417dbc1097237a449cbc604532c8c
school_app <eng.rakesh.thakral@gmail.com>
2026-06-15 14:06:10 +0530

    Added TransportRoute API CRUD operations

--- Controllers/TransportRouteController.cs
+++ Controllers/TransportRouteController.cs
@@ -78,33 +78,57 @@ namespace TransportApi.Controllers
         [HttpPost]
         public IActionResult Insert([FromBody] TransportRouteMaster model)
         {
-            using SqlConnection con = new SqlConnection(
-                _configuration.GetConnectionString("DefaultConnection"));
+            try
+            {
+                using SqlConnection con = new SqlConnection(
+                    _configuration.GetConnectionString("DefaultConnection"));
 
-            SqlCommand cmd = new SqlCommand(
-                "edu.sp_TransportRouteMaster_Insert",
-                con);
+                SqlCommand cmd = new SqlCommand(
+                    "edu.sp_TransportRouteMaster_Insert",
+                    con);
 
-            cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandType = CommandType.StoredProcedure;
 
-            cmd.Parameters.AddWithValue("@rCd", model.rCd);
-            cmd.Parameters.AddWithValue("@routeCode", model.routeCode);
-            cmd.Parameters.AddWithValue("@routeName", model.routeName);
-            cmd.Parameters.AddWithValue("@description", model.description);
-            cmd.Parameters.AddWithValue("@loginName", model.loginName);
-            cmd.Parameters.AddWithValue("@lUserDt", DateTime.Now.Date);
-            cmd.Parameters.AddWithValue("@UserID", model.UserID);
+                cmd.Parameters.AddWithValue("@rCd", model.rCd);
+                cmd.Parameters.AddWithValue("@routeCode", model.routeCode);
+                cmd.Parameters.AddWithValue("@routeName", model.routeName);
+                cmd.Parameters.AddWithValue("@description",
+                    (object?)model.description ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@loginName", model.loginName);
+                cmd.Parameters.AddWithValue("@lUserDt", DateTime.Now.Date);
+                cmd.Parameters.AddWithValue("@UserID", model.UserID);
 
-            con.Open();
+                con.Open();
 
-            SqlDataReader dr = cmd.ExecuteReader();
+                using SqlDataReader reader = cmd.ExecuteReader();
 
-            DataTable dt = new DataTable();
-            dt.Load(dr);
+                DataTable dt = new DataTable();
+                dt.Load(reader);
 
-            return Ok(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    return Ok(new
+                    {
+                        Status = Convert.ToInt32(dt.Rows[0]["Status"]),
+                        Message = dt.Rows[0]["Message"].ToString()
+                    });
+                }
+
+                return Ok(new
+                {
+                    Status = 0,
+                    Message = "No response received from database."
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    Status = 0,
+                    Message = ex.Message
+                });
+            }
         }
-
         [HttpPut("{id}")]
         public IActionResult Update(int id, [FromBody] TransportRouteMaster model)
         {
