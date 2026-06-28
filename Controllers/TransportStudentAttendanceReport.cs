7600d9d0ed42ea050d953d84990f79473523b5c2
school_app <eng.rakesh.thakral@gmail.com>
2026-06-28 20:29:24 +0530

    Updated transport modules and passed userId to pages

--- Controllers/TransportStudentAttendanceReport.cs
+++ Controllers/TransportStudentAttendanceReport.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using System.Data;
 
 namespace TransportApi.Controllers
@@ -16,56 +17,62 @@ namespace TransportApi.Controllers
         }
 
         // GET: api/TransportAttendance/TransportAttendanceReport
-        [HttpGet("TransportAttendanceReport")]
-        public IActionResult TransportAttendanceReport(
-     DateTime fromDate,
-     DateTime toDate,
-     int vCd = 0,
-     int routeId = 0,
-     int userId = 0)
+      
+}
+    [HttpGet("TransportAttendanceReport")]
+    public IActionResult TransportAttendanceReport(
+    DateTime fromDate,
+    DateTime toDate,
+    int vCd = 0,
+    int routeId = 0,
+    int userId = 0)
+    {
+        try
         {
-            try
+            List<object> list = new();
+
+            using SqlConnection con = new SqlConnection(
+                _configuration.GetConnectionString("DefaultConnection"));
+
+            using SqlCommand cmd = new SqlCommand("edu.TransportAttendanceRpt", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@FromDate", fromDate);
+            cmd.Parameters.AddWithValue("@ToDate", toDate);
+            cmd.Parameters.AddWithValue("@VCd", vCd);
+            cmd.Parameters.AddWithValue("@RouteID", routeId);
+            cmd.Parameters.AddWithValue("@UserID", userId);
+
+            con.Open();
+
+            using SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
             {
-                List<object> list = new();
-
-                using SqlConnection con = new SqlConnection(
-                    _configuration.GetConnectionString("DefaultConnection"));
-
-                using SqlCommand cmd = new SqlCommand("edu.TransportAttendanceRpt", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                cmd.Parameters.AddWithValue("@FromDate", fromDate);
-                cmd.Parameters.AddWithValue("@ToDate", toDate);
-                cmd.Parameters.AddWithValue("@VCd", vCd);
-                cmd.Parameters.AddWithValue("@RouteID", routeId);
-                cmd.Parameters.AddWithValue("@UserID", userId);
-
-                con.Open();
-
-                using SqlDataReader dr = cmd.ExecuteReader();
-
-                while (dr.Read())
+                list.Add(new
                 {
-                    list.Add(new
-                    {
-                        AttendanceID = dr["AttendanceID"],
-                        AdmCd = dr["AdmCd"],
-                        StudentName = dr["firstName"].ToString(),
-                        VCd = dr["vCd"].ToString(),
-                        RouteName = dr["RouteName"].ToString(),
-                        AttendanceDate = dr["AttendanceDate"],
-                        PickupTime = dr["PickupTime"],
-                        DropTime = dr["DropTime"],
-                        AttendanceStatus = dr["AttendanceStatus"]
-                    });
-                }
+                    AttendanceID = Convert.ToInt32(dr["AttendanceID"]),
+                    AdmCd = Convert.ToInt32(dr["AdmCd"]),
+                    StudentName = dr["StudentName"].ToString(),
+                    VehicleCode = Convert.ToInt32(dr["vCd"]),
+                    VehicleNo = dr["VehicleNo"].ToString(),
+                    RouteName = dr["RouteName"].ToString(),
+                    AttendanceDate = dr["AttendanceDate"] == DBNull.Value ? null : dr["AttendanceDate"],
+                    PickupTime = dr["PickupTime"] == DBNull.Value ? null : dr["PickupTime"],
+                    DropTime = dr["DropTime"] == DBNull.Value ? null : dr["DropTime"],
+                    AttendanceStatus = dr["AttendanceStatus"].ToString(),
+                    Message = dr["Message"].ToString()
+                });
+            }
 
-                return Ok(list);
-            }
-            catch (Exception ex)
+            return Ok(list);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new
             {
-                return BadRequest(ex.Message);
-            }
+                Success = false,
+                Error = ex.Message
+            });
         }
     }
-}
