1f915106b7e5f1c87b2a9015f1bb06d2b5c823be
school_app <eng.rakesh.thakral@gmail.com>
2026-07-10 09:38:36 +0530

    Add Subscription

---
+++ Controllers/SubscriptionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TransportApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriptionController : ControllerBase
+    {
+    }
+}
